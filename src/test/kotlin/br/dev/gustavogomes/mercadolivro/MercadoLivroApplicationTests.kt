262106c598a15a6ccccd2705a7460bf2baaff5c4
Gustavo Gomes <120190910+devgugga@users.noreply.github.com>
2024-03-05 12:44:37 -0300

    :tada: Commit Inicial
    Neste commit foi criada toda a estrutura inicial do projeto usando SpringBoot Initializer.

---
+++ src/test/kotlin/br/dev/gustavogomes/mercadolivro/MercadoLivroApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.dev.gustavogomes.mercadolivro
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MercadoLivroApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
